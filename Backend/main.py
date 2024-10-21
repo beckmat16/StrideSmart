099cac70bebb9270730abe6d54127376f8eb1037
Matthew Becker <matthewbecker@matthews-air-2.mynetworksettings.com>
2024-10-21 12:21:47 -0400

    Update for redirect

--- Backend/main.py
+++ Backend/main.py
@@ -23,7 +23,11 @@ FRONTEND_URL = os.getenv('FRONTEND_URL')
 ##Allow CORS for frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[os.getenv("FRONTEND_URL", "http://localhost:3000")],
+    allow_origins=[os.getenv("FRONTEND_URL"),
+                   "https://*.herokuapp.com",
+                   "http://localhost:3000",
+                   "http://localhost:3001"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
